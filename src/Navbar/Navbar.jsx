344fd8b58a6ba9287e1ddbaab374b695ec6dae85
rznayeem <riyazulnayeem@gmail.com>
2024-06-27 04:03:35 +0600

    final push

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,38 +1,3 @@
-// import { IoIosArrowDown } from 'react-icons/io';
-// import Logo from '../shared/Logo/Logo';
-// import call from '../assets/call.png';
-// import arrow from '../assets/arrow.png';
-// import './navbar.css';
-
-// const Navbar = () => {
-//   return (
-//     <nav className="nav">
-//       <Logo></Logo>
-//       <div className="nav-menu inter">
-//         <h3>Home</h3>
-//         <h3>About Us</h3>
-//         <h3>Services</h3>
-//         <h3>
-//           Pages <IoIosArrowDown />
-//         </h3>
-//         <h3>Blogs</h3>
-//         <h3>Contact</h3>
-//       </div>
-//       <div className="nav-end jakarta">
-//         <div className="call">
-//           <img src={call} alt="" />
-//           <h3 className="contact">+1 (888) 423-3526</h3>
-//         </div>
-//         <button>
-//           Get A Quote <img src={arrow} alt="" />
-//         </button>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import { useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
 import { FaBars, FaTimes } from 'react-icons/fa';
